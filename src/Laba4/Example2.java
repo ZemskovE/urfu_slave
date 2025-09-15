3eefb0de5ab12112c504b5ba6af81208a2106bc4
evgenii zemskov <zemskov-ed91@yandex.ru>
2025-09-15 21:23:58 +0500

    initial commit

---
+++ src/Laba4/Example2.java
@@ -0,0 +1,17 @@
+package Laba4;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int str = 11;
+        int length;
+        int i;
+        int z;
+
+        for (i=1; i <= str; i++) {
+            for (z=0; i > z; z++) {
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+    }
+}
