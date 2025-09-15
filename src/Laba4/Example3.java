f2ff0415bdcc113f91a5059d45043c80ddb52296
evgenii zemskov <zemskov-ed91@yandex.ru>
2025-09-15 21:23:49 +0500

    initial commit

---
+++ src/Laba4/Example3.java
@@ -0,0 +1,24 @@
+package Laba4;
+
+import java.util.Random;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int arr[][];
+        Random random = new Random();
+        int heigth = 4;
+        int length = 7;
+        arr = new int[heigth][length];
+        for (int i=0; i < heigth; i++) {
+            for (int j=0; j < length; j++) {
+                arr[i][j] = random.nextInt(1, 3);
+            }
+        }
+        for (int i=0; i < heigth; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
